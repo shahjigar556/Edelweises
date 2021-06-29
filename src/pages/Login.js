4077882b47a9e012af18cf9520d564418bc0e4e9
Jigar <shahjigar185@gmail.com>
2021-06-29 13:12:38 +0530

    updated Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,35 +1,27 @@
 import React from "react";
-
 import CssBaseline from "@material-ui/core/CssBaseline";
-
 import Checkbox from "@material-ui/core/Checkbox";
-
 import Grid from "@material-ui/core/Grid";
-
 import { Link } from "react-router-dom";
-
 import { makeStyles } from "@material-ui/core/styles";
-
 import { useState, useEffect } from "react";
 import Alert from "../components/Alert";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
+    backgroundColor: '#fff'
   },
   image: {
-    backgroundImage: "url(https://source.unsplash.com/random)",
+    //backgroundImage: "url(https://source.unsplash.com/random)",
     backgroundRepeat: "no-repeat",
-    backgroundColor:
-      theme.palette.type === "light"
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: "cover",
+    backgroundColor: "#F5F5F5",
     backgroundPosition: "center",
+    // height:'500px'
   },
   paper: {
     // margin: theme.spacing(8, 4),
-    marginTop: "15%",
+    marginTop: "12%",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
@@ -37,30 +29,36 @@ const useStyles = makeStyles((theme) => ({
   },
   input: {
     border: "none",
-    backgroundColor: "#EBEDF3",
+    paddingLeft:"10px",
+    backgroundColor: "#F5F5F5",
     borderRadius: "10px",
-    marginTop: "2px",
-    width: "65%",
+    width: "60%",
     height: "50px",
     [theme.breakpoints.down("sm")]: {
       width: "80%",
     },
+    border:'1px solid #ccc'
   },
   label: {
-    marginTop: "30px",
+    marginTop: "20px",
     fontFamily: "poppins",
-    fontSize: "20px",
-    fontWeight: "600",
+    fontSize: "18px",
+    fontWeight: "normal",
+    color: '#1A1934',
+
   },
   labelPassword: {
     marginTop: "10px",
     fontFamily: "poppins",
-    fontSize: "20px",
-    fontWeight: "600",
+    fontSize: "18px",
+    color: '#1A1934',
+    fontWeight: "normal",
   },
   forgotPassword: {
     color: "#3699FF",
-    marginLeft: "50%",
+    marginLeft: "44%",
+    textDecoration: 'none',
+    marginTop: '5px',
     [theme.breakpoints.down("sm")]: {
       marginLeft: "45%",
     },
@@ -70,22 +68,14 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "20px",
   },
   signInButton: {
-    backgroundColor: "#187DE4",
-    color: "white",
+    backgroundColor: "#1976D2",
+    color: "#fff",
+    marginTop: '10px',
     fontFamily: "poppins",
     fontWeight: "500",
     borderRadius: "5px",
     height: "40px",
-    width: "80px",
-    border: "none",
-  },
-  signInButtonGoogle: {
-    backgroundColor: "#E1F0FF",
-    marginLeft: "10px",
-    fontWeight: "500",
-    color: "#187DE4",
-    fontFamily: "poppins",
-    borderRadius: "5px",
+    width: "60%",
     border: "none",
   },
 }));
@@ -113,19 +103,24 @@ export default function Login() {
       <Alert show={open} message={message} count={count} />
       <Grid container component="main" className={classes.root}>
         <CssBaseline />
-        <Grid item xs={false} sm={4} md={4} className={classes.image} />
+        <Grid item xs={false} sm={4} md={3} className={classes.image} >
+
+        </Grid>
         <Grid item xs={12} sm={8} md={8}>
           <div className={classes.paper}>
-            <h2 style={{ fontFamily: "poppins", fontWeight: "600" }}>
-              Welcome To Loreum
+            <h2 style={{ fontFamily: "poppins", fontWeight: "600" , color: '#0A0A0A' }}>
+              Welcome To Thinkgestalt
             </h2>
+            <p style ={{width: '55%'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+               Duis fermentum id in elementum. Mauris adipiscing malesuada 
+               volutpat</p>
             <Link to="register" style={{ textDecoration: "none" }}>
               <p
                 style={{
-                  marginTop: "5px",
+                  marginTop: "0px",
                   marginBottom: "0px",
                   fontFamily: "poppins",
-                  fontWeight: "600",
+                  fontWeight: "normal",
                 }}
               >
                 New Here ?{" "}
@@ -139,27 +134,19 @@ export default function Login() {
                 className={classes.input}
                 type="email"
               ></input>
-              <p className={classes.forgotPassword}>Forgot Password</p>
               <p className={classes.labelPassword}>Password</p>
               <input
                 onChange={(e) => setPassword(e.target.value)}
                 className={classes.input}
                 type="password"
               ></input>
+              <Link to='/register' style={{ textDecoration: "none" }}><p className={classes.forgotPassword}>Forgot Password</p></Link>
               <div className={classes.btns}>
                 <button
                   onClick={submitHandler}
                   className={classes.signInButton}
                 >
                   SignIn
-                </button>
-                <button className={classes.signInButtonGoogle}>
-                  <img
-                    src="https://img.icons8.com/plasticine/100/000000/google-logo.png"
-                    style={{ height: "30px" }}
-                    className="img-fluid"
-                  />
-                  <span>Sign In Using Google</span>
                 </button>
               </div>
             </div>
